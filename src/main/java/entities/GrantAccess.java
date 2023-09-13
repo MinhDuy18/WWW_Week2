93ab494cef8df6b2a75f8f7a49eed5bea2b4f642
Minh Duy <duy19102018@gmail.com>
2023-09-13 20:24:13 +0700

    create entities

--- src/main/java/entities/GrantAccess.java
+++ src/main/java/entities/GrantAccess.java
@@ -1,4 +1,60 @@
 package entities;
 
 public class GrantAccess {
+    private Role role;
+    private Account account;
+    private boolean is_grant;
+    private String note;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public boolean isIs_grant() {
+        return is_grant;
+    }
+
+    public void setIs_grant(boolean is_grant) {
+        this.is_grant = is_grant;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public GrantAccess(Role role, Account account, boolean is_grant, String note) {
+        this.role = role;
+        this.account = account;
+        this.is_grant = is_grant;
+        this.note = note;
+    }
+
+    public GrantAccess() {
+    }
+
+    @Override
+    public String toString() {
+        return "GrantAccess{" +
+                "role=" + role +
+                ", account=" + account +
+                ", is_grant=" + is_grant +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
--- src/main/java/entities/Log.java
+++ src/main/java/entities/Log.java
@@ -1,4 +1,74 @@
 package entities;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 public class Log {
+    private int id;
+    private Account account;
+    private LocalDateTime login_time;
+    private LocalDateTime logout_time;
+    private String notes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public LocalDateTime getLogin_time() {
+        return login_time;
+    }
+
+    public void setLogin_time(LocalDateTime login_time) {
+        this.login_time = login_time;
+    }
+
+    public LocalDateTime getLogout_time() {
+        return logout_time;
+    }
+
+    public void setLogout_time(LocalDateTime logout_time) {
+        this.logout_time = logout_time;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Log(int id, Account account, LocalDateTime login_time, LocalDateTime logout_time, String notes) {
+        this.id = id;
+        this.account = account;
+        this.login_time = login_time;
+        this.logout_time = logout_time;
+        this.notes = notes;
+    }
+
+    public Log() {
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "id=" + id +
+                ", account=" + account +
+                ", login_time=" + login_time +
+                ", logout_time=" + logout_time +
+                ", notes='" + notes + '\'' +
+                '}';
+    }
 }
